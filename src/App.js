221e3e00a6f377b6dafadea6207b722f1a0df386
misternooblet <dragonroadsupport@gmail.com>
2023-03-01 15:47:08 +0200

    testing env on netlify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
+import { useEffect, useState } from "react";
 
+import { db } from "./firebase"
+
+import { query, collection, onSnapshot, updateDoc, doc } from "firebase/firestore";
+
+
+let tempCount = 0
 function App() {
+  const [counter, setCounter] = useState(10)
+  useEffect(() => {
+    const q = query(collection(db, 'counter'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        setCounter(doc.data().value);
+      })
+    })
+    return () => unsubscribe
+  }, [])
 
+  const toggleComplete = async () => {
+    tempCount += 1
+    await updateDoc(doc(db, 'counter', 'Shzydboy7y5X9xEOTrup'), {
+      value: tempCount,
+    });
+  };
 
   return (
-
     <div>
-
+      <h2>{counter}</h2>
+      <button onClick={toggleComplete}>inrease</button>
     </div>
-
   );
 }
 
 export default App;
+
+
+
+
+// function App() {
+
+
+//   return (
+
+//     <div>
+
+//     </div>
+
+//   );
+// }
+
+// export default App;
