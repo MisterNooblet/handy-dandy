c76e0872237e1309b72607813031936009a0b636
misternooblet <dragonroadsupport@gmail.com>
2023-03-01 17:47:20 +0200

    testing

--- src/firebase/fireBaseConfig.js
+++ src/firebase/fireBaseConfig.js
@@ -1,37 +1,37 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-let fbApiKey
-let fbAuthDomain
-let fbProjectId
-let fbStorageBucket
-let fbMessagingSenderId
-let fbAppId
+// let fbApiKey
+// let fbAuthDomain
+// let fbProjectId
+// let fbStorageBucket
+// let fbMessagingSenderId
+// let fbAppId
 
-if (process.env.NODE_ENV !== 'production') {
-    fbApiKey = process.env.REACT_APP_API_KEY
-    fbAuthDomain = process.env.REACT_APP_AUTH_DOMAIN
-    fbProjectId = process.env.REACT_APP_PROJECT_ID
-    fbStorageBucket = process.env.REACT_APP_STORAGE_BUCKET
-    fbMessagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID
-    fbAppId = process.env.REACT_APP_APP_ID
-} else {
-    fbApiKey = process.env.API_KEY
-    fbAuthDomain = process.env.AUTH_DOMAIN
-    fbProjectId = process.env.PROJECT_ID
-    fbStorageBucket = process.env.STORAGE_BUCKET
-    fbMessagingSenderId = process.env.MESSAGING_SENDER_ID
-    fbAppId = process.env.APP_ID
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     fbApiKey = process.env.REACT_APP_API_KEY
+//     fbAuthDomain = process.env.REACT_APP_AUTH_DOMAIN
+//     fbProjectId = process.env.REACT_APP_PROJECT_ID
+//     fbStorageBucket = process.env.REACT_APP_STORAGE_BUCKET
+//     fbMessagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID
+//     fbAppId = process.env.REACT_APP_APP_ID
+// } else {
+//     fbApiKey = process.env.API_KEY
+//     fbAuthDomain = process.env.AUTH_DOMAIN
+//     fbProjectId = process.env.PROJECT_ID
+//     fbStorageBucket = process.env.STORAGE_BUCKET
+//     fbMessagingSenderId = process.env.MESSAGING_SENDER_ID
+//     fbAppId = process.env.APP_ID
+// }
 
 
 const firebaseConfig = {
-    apiKey: fbApiKey,
-    authDomain: fbAuthDomain,
-    projectId: fbProjectId,
-    storageBucket: fbStorageBucket,
-    messagingSenderId: fbMessagingSenderId,
-    appId: fbAppId
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 
