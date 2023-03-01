8085e1e9a3b9b8e872bc83c74014a269dc344027
misternooblet <dragonroadsupport@gmail.com>
2023-03-01 19:41:26 +0200

    removed unused imports

--- src/pages/LayoutComponent/LayoutComponent.js
+++ src/pages/LayoutComponent/LayoutComponent.js
@@ -1,4 +1,4 @@
-import { Box, Container, display } from '@mui/system'
+import { Box, Container } from '@mui/system'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
