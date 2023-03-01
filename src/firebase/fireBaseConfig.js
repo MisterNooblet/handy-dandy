00a7f776d611f0bca62aeb572d45d8a3f7025cb9
misternooblet <dragonroadsupport@gmail.com>
2023-03-01 17:50:15 +0200

    env works

--- src/firebase/fireBaseConfig.js
+++ src/firebase/fireBaseConfig.js
@@ -1,30 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// let fbApiKey
-// let fbAuthDomain
-// let fbProjectId
-// let fbStorageBucket
-// let fbMessagingSenderId
-// let fbAppId
-
-// if (process.env.NODE_ENV !== 'production') {
-//     fbApiKey = process.env.REACT_APP_API_KEY
-//     fbAuthDomain = process.env.REACT_APP_AUTH_DOMAIN
-//     fbProjectId = process.env.REACT_APP_PROJECT_ID
-//     fbStorageBucket = process.env.REACT_APP_STORAGE_BUCKET
-//     fbMessagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID
-//     fbAppId = process.env.REACT_APP_APP_ID
-// } else {
-//     fbApiKey = process.env.API_KEY
-//     fbAuthDomain = process.env.AUTH_DOMAIN
-//     fbProjectId = process.env.PROJECT_ID
-//     fbStorageBucket = process.env.STORAGE_BUCKET
-//     fbMessagingSenderId = process.env.MESSAGING_SENDER_ID
-//     fbAppId = process.env.APP_ID
-// }
-
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -33,8 +9,6 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
