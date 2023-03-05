3920837976267fc6e5f2d7255e6ffe4f28c7e296
misternooblet <dragonroadsupport@gmail.com>
2023-03-05 17:26:24 +0200

    first page layout

--- src/pages/Wiki/Wiki.page.js
+++ src/pages/Wiki/Wiki.page.js
@@ -12,85 +12,109 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [
+  {
+    title: 'Tools',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis cupiditate voluptatum reiciendis ipsa in impedit possimus nostrum iure autem incidunt?',
+    image:
+      'https://www.structuralguide.com/wp-content/uploads/2022/01/Construction-Materials.jpg',
+  },
+  {
+    title: 'Materials',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis cupiditate voluptatum reiciendis ipsa in impedit possimus nostrum iure autem incidunt?',
+    image:
+      'https://www.letsbuild.com/wp-content/uploads/2017/06/tools-864983_1280.jpg',
+  },
+];
 
 const theme = createTheme();
 
 export default function Wiki() {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <>
-                {/* Hero unit */}
-                <Box
-                    sx={{
-                        bgcolor: 'background.paper',
-                        pt: 8,
-                        pb: 6,
-                    }}
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Our Tool-O-Pedia
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Here you will find information about diffrent tools and materials
+              mentioned around in our application and articles. So go ahead and
+              select a category below , to start exploring.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              {/* <Button variant="contained">Main call to action</Button>
+                            <Button variant="outlined">Secondary action</Button> */}
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid
+            container
+            sx={{ display: 'flex', justifyContent: 'space-around' }}
+          >
+            {cards.map((card) => (
+              <Grid item key={card.title} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
                 >
-                    <Container maxWidth="sm">
-                        <Typography
-                            component="h1"
-                            variant="h2"
-                            align="center"
-                            color="text.primary"
-                            gutterBottom
-                        >
-                            Our Tool-O-Pedia
-                        </Typography>
-                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                            Here you will find information about diffrent tools and materials mentioned
-                            around in our application and articles.
-                            So go ahead and select a category below , to start exploring.
-                        </Typography>
-                        <Stack
-                            sx={{ pt: 4 }}
-                            direction="row"
-                            spacing={2}
-                            justifyContent="center"
-                        >
-                            <Button variant="contained">Main call to action</Button>
-                            <Button variant="outlined">Secondary action</Button>
-                        </Stack>
-                    </Container>
-                </Box>
-                <Container sx={{ py: 8 }} maxWidth="md">
-                    {/* End hero unit */}
-                    <Grid container spacing={4}>
-                        {cards.map((card) => (
-                            <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card
-                                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                                >
-                                    <CardMedia
-                                        component="img"
-                                        sx={{
-                                            // 16:9
-                                            pt: '56.25%',
-                                        }}
-                                        image="https://source.unsplash.com/random"
-                                        alt="random"
-                                    />
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Heading
-                                        </Typography>
-                                        <Typography>
-                                            This is a media card. You can use this section to describe the
-                                            content.
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small">View</Button>
-                                        <Button size="small">Edit</Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
-            </>
-        </ThemeProvider>
-    );
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '10.25%',
+                    }}
+                    image={card.image}
+                    alt={card.title}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.title}
+                    </Typography>
+                    <Typography>{card.description}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </>
+    </ThemeProvider>
+  );
 }
