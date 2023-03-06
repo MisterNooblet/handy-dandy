8a4285a4e99096a098df82ba2903ce6d90568118
misternooblet <dragonroadsupport@gmail.com>
2023-03-06 23:39:36 +0200

    end of day

--- src/pages/Profile/Profile.page.js
+++ src/pages/Profile/Profile.page.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+    const user = useSelector((state) => state.auth)
+    console.log(user);
     return (
-        <div>Profile</div>
+        <>
+            <div>{user.user.displayName}</div>
+            <div>{user.user.email}</div>
+        </>
     )
 }
 
