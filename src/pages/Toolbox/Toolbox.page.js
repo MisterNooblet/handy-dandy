3f92ed72157b8c45723d4f9fe581b5f556670d07
misternooblet <dragonroadsupport@gmail.com>
2023-03-06 19:51:05 +0200

    toolbox list

--- src/pages/Toolbox/Toolbox.page.js
+++ src/pages/Toolbox/Toolbox.page.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
 
 
 const Toolbox = () => {
     const user = useSelector((state) => state.auth)
+    const toolbox = user.userExtras.toolbox
     console.log(user.userExtras);
+
+
+    const [checked, setChecked] = React.useState([0]);
+
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
+
+
 
     if (!user.user) {
         return (
@@ -15,7 +41,38 @@ const Toolbox = () => {
     } else {
         return (
             <>
-                {user.userExtras.toolbox.length === 0 ? <div>Toolbox empty</div> : null}
+                {toolbox.length === 0 ? <div>Toolbox empty</div> : (
+                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                        {toolbox.map((value) => {
+                            const labelId = `checkbox-list-label-${value.name}`;
+
+                            return (
+                                <ListItem
+                                    key={value.name}
+                                    secondaryAction={
+                                        <IconButton edge="end" aria-label="comments">
+                                            <InfoIcon />
+                                        </IconButton>
+                                    }
+                                    disablePadding
+                                >
+                                    <ListItemButton role={undefined} onClick={handleToggle(value.name)} dense>
+                                        <ListItemIcon>
+                                            <Checkbox
+                                                edge="start"
+                                                checked={checked.indexOf(value.name) !== -1}
+                                                tabIndex={-1}
+                                                disableRipple
+                                                inputProps={{ 'aria-labelledby': labelId }}
+                                            />
+                                        </ListItemIcon>
+                                        <ListItemText id={labelId} primary={`${value.name}`} />
+                                    </ListItemButton>
+                                </ListItem>
+                            );
+                        })}
+                    </List>
+                )}
             </>
         )
     }
