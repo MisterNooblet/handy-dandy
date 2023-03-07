7b11912fa17d0efd1f81631d81a1f69621c04363
misternooblet <dragonroadsupport@gmail.com>
2023-03-07 20:48:40 +0200

    ???

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 import dataFetcher from "./dataFetcher";
 import fireBaseAuth from "./fireBaseAuth";
-import toolFetcher from "./toolFetcher";
+import toolFetcher from "./toolFetcher.js";
 
 export { dataFetcher, toolFetcher, fireBaseAuth }
