6b95980836007ec00b4ea34bdb37b1be55493a07
misternooblet <dragonroadsupport@gmail.com>
2023-03-08 14:05:36 +0200

    popups on article

--- src/pages/Home/components/Article.js
+++ src/pages/Home/components/Article.js
@@ -1,8 +1,34 @@
-import { Card, Typography } from '@mui/material'
+import { Button, Card, List, ListItem, Typography } from '@mui/material'
 import { Box, } from '@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import ItemPopup from './ItemPopup'
+import InfoIcon from '@mui/icons-material/Info';
+
 
 const Article = ({ article }) => {
+    const [open, setOpen] = useState(false);
+    const [currentItem, setCurrentItem] = useState(null)
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+
+    const user = useSelector((state) => state.auth)
+
+    const handleOpenPopup = (item) => {
+        setCurrentItem(prev => prev = item)
+        handleClickOpen()
+    }
+
+    const userHasTool = (item) => {
+        if (user.user && item) {
+            const userOwns = user.userExtras.toolbox.find(tool => tool.name === item.name)
+            return userOwns ? true : false
+        }
+        return false
+    }
+
     return (
         <Card sx={{ padding: 5, display: 'grid', gridTemplateColumns: 'repeat(9, 1fr)', gridTemplateRows: 'repeat(12, 1fr)' }}>
             <Typography textAlign={'center'} component={'h3'} variant='h3' sx={{ gridArea: '1 / 1 / 2 / 10' }} >
@@ -12,21 +38,34 @@ const Article = ({ article }) => {
             <Typography sx={{ gridArea: '2/4/13/10' }}>{article.description}</Typography>
             <Box sx={{ gridArea: '5 / 1 / 8 / 4' }}>
                 <Typography>Materials Required:</Typography>
-                <ul>
-                    {article.materials.map(item => <li key={item.name}>{item.name}</li>)}
-                </ul>
+                <List>
+                    {article.materials.map(item => {
+                        if (user.user) {
+                            return <ListItem onClick={() => handleOpenPopup(item)} sx={{ color: userHasTool(item) ? 'green' : "red", cursor: 'pointer' }} key={item.name}>{item.name}<InfoIcon /></ListItem>
+                        } else {
+                            return <ListItem onClick={() => handleOpenPopup(item)} key={item.name}>{item.name}<InfoIcon /></ListItem>
+                        }
+                    })}
+                </List>
             </Box>
             <Box sx={{ gridArea: '8 / 1 / 11 / 4' }}>
                 <Typography>Tools Required:</Typography>
-                <ul>
-                    {article.tools.map(item => <li key={item.name}>{item.name}</li>)}
-                </ul>
+                <List>
+                    {article.tools.map(item => {
+                        if (user.user) {
+                            return <ListItem onClick={() => handleOpenPopup(item)} sx={{ color: userHasTool(item) ? 'green' : "red", cursor: 'pointer' }} key={item.name}>{item.name}<InfoIcon /></ListItem>
+                        } else {
+                            return <ListItem onClick={() => handleOpenPopup(item)} key={item.name}>{item.name}<InfoIcon /></ListItem>
+                        }
+                    })}
+                </List>
             </Box>
             <Box sx={{ gridArea: '11 / 1 / 13 / 4' }}>
                 <Typography>Our Tips for the job:</Typography>
                 <ul>
                     {article.props.map(item => <li key={item}>{item}</li>)}
                 </ul>
+                <ItemPopup open={open} setOpen={setOpen} item={currentItem} />
             </Box>
         </Card>
     )
